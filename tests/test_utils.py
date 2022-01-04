2f22decc0f182a21f809a8cf1af68e0423bf8a7c
limon8884 <dan.limon.db@gmail.com>
2022-01-04 18:29:45 +0300

    commit some unknown stuff

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,11 @@
 import pytest
-from utils import compute_total_J
+from utils import compute_total_J, compute_acceleration
 
 def test_compute_total_J():
     compute_total_J(True)
+
+def test_compute_acceleration1():
+    compute_acceleration(5., 10., compute_total_J())
+
+def test_compute_acceleration2():
+    assert compute_acceleration(5., 5., compute_total_J()) == 0
