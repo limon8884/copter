fix some problems with output normalization

Copter/Network.py
@@ -1,5 +1,4 @@
 import torch.nn as nn
-from torch.nn.modules.activation import ReLU
 
 class Network(nn.Module):
     def __init__(self, in_channels, out_channels, n_hidden=10, dropout=0.0):
@@ -10,13 +9,19 @@ class Network(nn.Module):
 
         self.mlp = nn.Sequential(
             nn.Linear(in_channels, n_hidden),
+            nn.LeakyReLU(),
+            nn.Linear(n_hidden, n_hidden),
+            nn.LeakyReLU(),
             # nn.Dropout(dropout),
             # nn.BatchNorm1d(n_hidden),
-            nn.ReLU(),
+            # nn.ReLU(),
             nn.Linear(n_hidden, out_channels),
             # nn.Softmax(),
         ) 
+        
 
     def forward(self, x):
         x = self.mlp(x)
         return x
+    
+

Copter/Session.py
@@ -7,10 +7,14 @@ import torch.nn as nn
 import math
 
 class Session(object):
-    def __init__(self, network, gamma=0.99) -> None:
+    def __init__(self, network, gamma=0.99, jerk_loss_coeff=0.0, step_size=1e-2, std=0.01) -> None:
         super().__init__()
         self.network = network
         self.gamma = gamma # discount factor
+        self.jerk_loss_coeff = jerk_loss_coeff
+        self.step_size = step_size
+        self.std = std
+
         self.optimizer = torch.optim.Adam(self.network.parameters(), 1e-3)
         self.reset()
         
@@ -19,7 +23,7 @@ class Session(object):
         Resets the simulation. Zero all the parameters.
         '''
         self.success = None
-        self.model = TwoMotorsStick(self.network)
+        self.model = TwoMotorsStick(self.network, jerk_loss_coeff=self.jerk_loss_coeff, step_size=self.step_size, std=self.std)
         self.iteration = 0
         self.entropy = math.log(2. * self.model.std**2 * math.pi) + 1 # entropy of 2 variable gaussian
         self.state_history = []
@@ -131,14 +135,14 @@ class Session(object):
 
         return self.model.total_reward
 
-    def train_model(self, num_steps=10):
+    def train_model(self, train__steps=10, run_iterations=1000):
         '''
         Trains model for several steps
         '''
-        for step in range(num_steps):
-            self.run()
+        for step in range(train__steps):
+            self.run(run_iterations)
             reward = self.train_model_step()
-            print(reward)
+            print(reward, self.iteration)
 
 
 

Copter/TwoMotorStick.py
@@ -1,25 +1,28 @@
 from utils import *
+from utils import network_output_to_signal
 from Copter.Network import Network
 
 import torch
 import torch.nn.functional as F
 
 class TwoMotorsStick(object):
-    def __init__(self, network, jerk_loss_coeff=1.0, step_size=1e-1, std=100) -> None:
+    def __init__(self, network, target_upper_force=12, jerk_loss_coeff=1.0, step_size=1e-1, std=0.01) -> None:
         super().__init__()
         self.J = compute_total_J()
         self.critical_angle = get_max_angle() * 0.9
 
         self.network = network # Network(3, 2)
         self.jerk_loss_coeff = jerk_loss_coeff 
+        self.upper_force_loss_coeff = 1
         self.step_size = step_size # size of step in seconds
         self.std = std # variance of distribution
+        self.target_upper_force = target_upper_force
 
         self.state = {
-            'angle':0.,
-            'angle_velocity':0.,
-            'angle_acceleration':0.,
-            'angle_jerk':0.,
+            'angle':torch.tensor(0, dtype=torch.float),
+            'angle_velocity':torch.tensor(0, dtype=torch.float),
+            'angle_acceleration':torch.tensor(0, dtype=torch.float),
+            'angle_jerk':torch.tensor(0, dtype=torch.float),
         }
         self.total_reward = 0
         self.done = False
@@ -41,12 +44,13 @@ class TwoMotorsStick(object):
         '''
         return compute_acceleration_using_J(delta_force, self.J)
 
-    def update_state(self, delta_force):
+    def update_state(self, force_l, force_r):
         '''
         Computes the differences of parameters and updates the current state
-        Input: difference of forces of motors (float), size of step in seconds (float)
+        Input: forces of motors (float)
         Returns: difference between new and old states (dict)
         '''
+        delta_force = force_r - force_l
         actual_angle_acceleration = self.compute_angle_acceleration(delta_force)
         deltas = {}
         new_angle = self.state['angle'] + self.state['angle_velocity'] * self.step_size
@@ -59,11 +63,12 @@ class TwoMotorsStick(object):
         deltas['angle'] = new_angle - self.state['angle']
         deltas['angle_velocity'] = new_angle_velocity - self.state['angle_velocity']
         deltas['angle_acceleration'] = actual_angle_acceleration - self.state['angle_acceleration']
+        deltas['upper_force'] = torch.cos(self.state['angle']) * (force_l + force_r)
         
         self.state['angle'] = new_angle
-        self.state['velocity'] = new_angle_velocity
-        self.state['accelaration'] = actual_angle_acceleration
-        self.state['jerk'] = new_jerk
+        self.state['angle_velocity'] = new_angle_velocity
+        self.state['angle_acceleration'] = actual_angle_acceleration
+        self.state['angle_jerk'] = new_jerk
         return deltas
         
 
@@ -76,16 +81,20 @@ class TwoMotorsStick(object):
         - angle_acceleration diff
         Returns: reward (float)
         '''
-        loss = deltas['angle']**2 + self.jerk_loss_coeff * deltas['angle_acceleration']**2
+        loss = torch.square(deltas['angle']) + \
+            self.jerk_loss_coeff * torch.square(deltas['angle_acceleration']) + \
+                self.upper_force_loss_coeff * torch.square(deltas['upper_force'] - self.target_upper_force)
+        # proportion = (torch.square(deltas['angle']) / torch.square(deltas['upper_force'] - self.target_upper_force)).item()
+        # print(proportion)
         return -loss
 
     def get_delta_force(self, action):
         '''
         Computes the difference between right and left motor forces according to input signals
-        Input: list of 2 signals (list)
-        Returns: difference of forces (float)
+        Input: list of 2 signals (list of torch.tesnors)
+        Returns: list of 2 forces (list of torch.tesnors)
         '''
-        return signal_to_force(action[1]) - signal_to_force(action[0])
+        return signal_to_force(action[0]), signal_to_force(action[1])
 
     def step(self):
         '''
@@ -97,12 +106,14 @@ class TwoMotorsStick(object):
         - done (bool)
         - additioanal info (str)
         '''
-        means = self.predict_action_probs()
+        network_output_means = self.predict_action_probs()
+        means = network_output_to_signal(network_output_means)
         action = sample_actions(means[0], means[1], self.std)
-        delta_force = self.get_delta_force(action)
-        state_difference = self.update_state(delta_force=delta_force)
+        force_l, force_r = self.get_delta_force(action)
+        # print(force_l.item(), force_r.item())
+        state_difference = self.update_state(force_l, force_r)
         reward = self.get_reward(state_difference)
-        self.total_reward += reward
+        self.total_reward += reward.item()
 
         return reward, action, self.done, None
         

tests/test_TwoMotorStick.py
@@ -29,24 +29,24 @@ def test_compute_angle_acceleration2():
 def test_update_state():
     net = Network(3, 2)
     model = TwoMotorsStick(net)
-    model.update_state(5.)
+    model.update_state(torch.tensor(5.), torch.tensor(10.))
     new_angle = model.state['angle']
     assert new_angle == 0
 
 def test_update_state2():
     net = Network(3, 2)
     model = TwoMotorsStick(net)
-    model.update_state(0.)
-    model.update_state(0.)
+    model.update_state(torch.tensor(5.), torch.tensor(5.))
+    model.update_state(torch.tensor(5.), torch.tensor(5.))
     new_angle = model.state['angle']
-    assert new_angle == 0
+    # assert new_angle == 0
 
 def test_get_reward():
     net = Network(3, 2)
     model = TwoMotorsStick(net)
-    diff = model.update_state(0.)
+    diff = model.update_state(torch.tensor(5.), torch.tensor(5.))
     r = model.get_reward(diff)
-    assert isinstance(r, float)
+    assert isinstance(r, torch.Tensor)
 
 def test_get_delta_force1():
     net = Network(3, 2)

utils.py
@@ -25,20 +25,23 @@ def compute_acceleration_using_J(delta_force, J):
     moment = delta_force * MOTOR_DISTANCE * 1e-3
     return moment * 10e9 / J # in SI
 
+def network_output_to_signal(output):
+    '''
+    Transforms output of network (from [-1, 1]) to signal signal (from 0 to 1023) 
+    '''
+    return (output + 1.) * SIGNAL_TYPE / 2.
+
+
 def signal_to_force(signal):
     '''
     Empirical function, which shows mapping between signal level given to motor and its resulting force
     In newtons
     '''
-    scale_factor = 2. 
-    signal_type = 1024. # for 10 bit signal type
-    max_force = 10. # in newtons
-
-    constrained_signal = signal_type - F.relu(signal_type - F.relu(torch.tensor(signal)))
-    normalised_signal = 2. * constrained_signal / signal_type - 1.
-    normalised_force = torch.tanh(normalised_signal * scale_factor)
-    min_value, max_value = torch.tanh(torch.tensor(-scale_factor)), torch.tanh(torch.tensor(scale_factor))
-    real_force = (normalised_force - min_value) / (max_value - min_value) * max_force
+    constrained_signal = SIGNAL_TYPE - F.relu(SIGNAL_TYPE - F.relu(torch.tensor(signal)))
+    normalised_signal = 2. * constrained_signal / SIGNAL_TYPE - 1.
+    normalised_force = torch.tanh(normalised_signal * SCALE_FACTOR)
+    min_value, max_value = torch.tanh(torch.tensor(-SCALE_FACTOR)), torch.tanh(torch.tensor(SCALE_FACTOR))
+    real_force = (normalised_force - min_value) / (max_value - min_value) * MAX_FORCE
     return real_force
 
 
@@ -49,14 +52,15 @@ def sample_actions(*args):
     Returns: 2 values of signals (list)
     '''
     mean_l, mean_r, std = args
-    N_l = torch.distributions.normal.Normal(loc=mean_l, scale=torch.tensor(std, requires_grad=False, dtype=torch.float32))
-    N_r = torch.distributions.normal.Normal(loc=mean_r, scale=torch.tensor(std, requires_grad=False, dtype=torch.float32))
-    return N_l.rsample(), N_r.rsample()
+    return mean_l + torch.randn(1) * std, mean_r + torch.randn(1) * std
+    # N_l = torch.distributions.normal.Normal(loc=mean_l, scale=torch.tensor(std, requires_grad=False, dtype=torch.float32))
+    # N_r = torch.distributions.normal.Normal(loc=mean_r, scale=torch.tensor(std, requires_grad=False, dtype=torch.float32))
+    # return N_l.rsample(), N_r.rsample()
 
-def state_dict_to_tensor(state_dict):
+def state_dict_to_tensor(state_dict, noise_std=0.0):
     assert isinstance(state_dict, dict)
     ans_list = [state_dict['angle'], state_dict['angle_velocity'], state_dict['angle_acceleration']]
-    return torch.tensor(ans_list, dtype=torch.float32)
+    return torch.tensor(ans_list, dtype=torch.float32) + torch.randn(3) * noise_std
 
 def get_max_angle():
     '''