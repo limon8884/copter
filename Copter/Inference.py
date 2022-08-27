d85da719f071f5a4e2c0a739af774ab39201c579
limon8884 <dan.limon.db@gmail.com>
2022-08-28 01:15:15 +0300

    realized Inference class. Connecting.ino +- ready

--- Copter/Inference.py
+++ Copter/Inference.py
@@ -1,31 +1,50 @@
-from sqlite3 import TimeFromTicks
+# from sqlite3 import TimeFromTicks
+import torch
 import serial
 import time
 
-
-
 class Inference():
     def __init__(self, network) -> None:
-        self.arduino = serial.Serial(port='COM10', baudrate=115200, timeout=.1)
+        self.arduino = serial.Serial(port='COM10', baudrate=115200, timeout=0.1)
         self.network = network
+        time.sleep(1)
 
-    def transmit(self, signal):
-        self.arduino.write(bytes(signal, 'utf-8')) # надо бы флаты в байты обернуть
+    def transmit(self, signal, sleep_time=0.01):
+        '''
+        Input: list of integers from 0 to 255
+        '''
+        self.arduino.write(bytes(list(signal)))
+        time.sleep(sleep_time)
 
     def recieve(self):
-        return self.arduino.readline()
+        if self.arduino.in_waiting > 0:
+            s = self.arduino.readline(self.arduino.in_waiting).decode('utf-8')
+            vals = list(map(int, s.split(',')))
+            assert len(vals) == 3, vals
+            return torch.tensor(vals)
+        return None
 
-    def run(self):
-        current_time = 0
-        for i in range(10000):
-            current_time += time.time()
-            data_in = self.recieve() # надо бы кастануть
-            data_out = self.network(data_in)
-            time.sleep(0.001)
+    def run(self, n_iters=1000):
+        time.sleep(2)
+        self.arduino.readline(self.arduino.in_waiting)
+        self.transmit([0, 0])
+        current_time = time.time()
+        for i in range(n_iters):
+            # print('iter: ', i)
+            while self.arduino.in_waiting == 0:
+                pass
+            # current_time += time.time()
+            data_in = self.recieve() 
+            # data_out = self.network(data_in)
+            data_out = [129, 127]
             self.transmit(data_out)
-            self.arduino.flush() # здесь ли?
 
             if i % 100 == 0:
                 print('1 iteration time: ', (time.time() - current_time) / 100)
-                current_time = 0
+                current_time = time.time()
+
+        self.arduino.close()
+
+    def stop(self):
+        self.arduino.close()
 
--- arduino sketchs/connecting/connecting.ino
+++ arduino sketchs/connecting/connecting.ino
@@ -1,10 +1,28 @@
-int x;
+
+const byte numSignals = 2;
+byte signals[numSignals];
+const int delay_time = 20; // in millis
+
 void setup() {
- Serial.begin(115200);
- Serial.setTimeout(1);
+    Serial.begin(115200);
+    Serial.setTimeout(1);
 }
+
 void loop() {
- while (!Serial.available());
- x = Serial.readString().toInt();
- Serial.print(x + 1);
+    while (!Serial.available());
+    if (Serial.available())
+    {    
+        Serial.readBytes(signals, numSignals);
+        // send signals to motors
+
+        // get data from env 
+
+        int angle = int(signals[0] + 1);
+        int velocity = int(signals[1] + 1);
+        int acceleration = int(signals[1] + 10);
+
+        String s = String(angle) + "," + String(velocity) + "," + String(acceleration);
+        Serial.print(s);
+        delay(delay_time);
+    }
 }
