0de206991e7bf79a3fd031a7c89975f9ab3538b9
limon8884 <dan.limon.db@gmail.com>
2022-08-29 23:35:31 +0300

    connecting.ino - releazed w/o motors

--- Copter/Inference.py
+++ Copter/Inference.py
@@ -2,12 +2,22 @@
 import torch
 import serial
 import time
+import numpy as np
 
 class Inference():
     def __init__(self, network) -> None:
         self.arduino = serial.Serial(port='COM10', baudrate=115200, timeout=0.1)
         self.network = network
         time.sleep(1)
+        self.logs = {
+            'angle_dmp': [], # in degrees
+            'angle_pot': [], # in analog signal
+            'angle_velocity': [], # in radians/s
+            'acceleration': [], # raw, with distance and gravity. In m/s^2
+            'time': [], # in seconds
+            'debug': [], # signal1 + 1
+        }
+        self.fuckup_iterations = 0
 
     def transmit(self, signal, sleep_time=0.01):
         '''
@@ -19,29 +29,47 @@ class Inference():
     def recieve(self):
         if self.arduino.in_waiting > 0:
             s = self.arduino.readline(self.arduino.in_waiting).decode('utf-8')
-            vals = list(map(int, s.split(',')))
-            assert len(vals) == 3, vals
-            return torch.tensor(vals)
-        return None
+            vals = list(map(float, s.split(',')))
+            if len(vals) != 5:
+                self.fuckup_iterations += 1
+                return [None] * 5
 
-    def run(self, n_iters=1000):
-        time.sleep(2)
+            return vals
+        return [None] * 5
+
+    def flush(self, delay=0.01):
         self.arduino.readline(self.arduino.in_waiting)
         self.transmit([0, 0])
+        time.sleep(delay)
+
+    def run(self, n_iters=1000):
+        self.flush(delay=5)
+        for _ in range(5):
+            self.flush()
+            
         current_time = time.time()
         for i in range(n_iters):
-            # print('iter: ', i)
             while self.arduino.in_waiting == 0:
                 pass
-            # current_time += time.time()
             data_in = self.recieve() 
+
+
+            self.logs['angle_dmp'].append(data_in[0])
+            self.logs['angle_pot'].append(data_in[1])
+            self.logs['angle_velocity'].append(data_in[2])
+            self.logs['acceleration'].append(data_in[3])
+            self.logs['time'].append(time.time() - current_time)
+            self.logs['debug'].append(data_in[4])
+
+            current_time = time.time()
             # data_out = self.network(data_in)
             data_out = [129, 127]
             self.transmit(data_out)
 
             if i % 100 == 0:
-                print('1 iteration time: ', (time.time() - current_time) / 100)
-                current_time = time.time()
+                # print(data_in)
+                print('1 iteration time: ', np.mean(self.logs['time'][-100:]))
+                # current_time = time.time()
 
         self.arduino.close()
 
--- arduino sketchs/connecting/connecting.ino
+++ arduino sketchs/connecting/connecting.ino
@@ -1,28 +1,81 @@
+// BACKENG FOR ARDUINO MODULE CONNECTED TO INFERENCE
+
+#include "I2Cdev.h"
+#include "MPU6050_6Axis_MotionApps20.h"
+MPU6050 mpu;
+
+volatile bool mpuFlag = false;  // флаг прерывания готовности
+uint8_t fifoBuffer[45];         // буфер
+int16_t ax, ay, az;
+int16_t gx, gy, gz;
 
 const byte numSignals = 2;
-byte signals[numSignals];
-const int delay_time = 20; // in millis
+byte signals[numSignals]; // input signals buffer
+
+const float NONE_CONST = 100.0; // fake constant for angle (like None)
+
+const int pot = A0; // potentiometer pin
 
 void setup() {
-    Serial.begin(115200);
-    Serial.setTimeout(1);
+  Serial.begin(115200);
+  pinMode(pot, INPUT);
+  Wire.begin();
+  //Wire.setClock(1000000UL);     // разгоняем шину на максимум
+
+  mpu.initialize();
+  mpu.setFullScaleGyroRange(MPU6050_GYRO_FS_500);
+  mpu.CalibrateAccel(6);
+  mpu.CalibrateGyro(6);
+
+  // инициализация DMP и прерывания
+  mpu.dmpInitialize();
+  mpu.setDMPEnabled(true);
+  attachInterrupt(0, dmpReady, RISING);
+  delay(2000);
+}
+
+// прерывание готовности. Поднимаем флаг
+void dmpReady() {
+  mpuFlag = true;
 }
 
 void loop() {
-    while (!Serial.available());
-    if (Serial.available())
+  float current_angle = NONE_CONST;
+  // по флагу прерывания и готовности DMP
+  if (mpuFlag && mpu.dmpGetCurrentFIFOPacket(fifoBuffer)) {
+    // переменные для расчёта (ypr можно вынести в глобал)
+    Quaternion q;
+    VectorFloat gravity;
+    float ypr[3];
+
+    // расчёты
+    mpu.dmpGetQuaternion(&q, fifoBuffer);
+    mpu.dmpGetGravity(&gravity, &q);
+    mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+    mpuFlag = false;
+
+    current_angle = ypr[0] / 3.1415 * 180;
+    delay(15);
+  }
+  if (Serial.available())
     {    
+        // get data from agent
         Serial.readBytes(signals, numSignals);
+        // get data from env
+        mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+        int pot_val = analogRead(pot);
+        
         // send signals to motors
+        // RELEAZE
 
-        // get data from env 
-
-        int angle = int(signals[0] + 1);
-        int velocity = int(signals[1] + 1);
-        int acceleration = int(signals[1] + 10);
-
-        String s = String(angle) + "," + String(velocity) + "," + String(acceleration);
+        String s = String(current_angle) // angle in degrees
+            + "," + String(pot_val) // analog signal from angle potentiometer (from 0 to 2023)
+            + "," + String((float)gz / 32768 * 500) // angle velocity (radians/s)
+            + "," + String((float)ay / 32768 * 2) // acceleration (m/s^2)
+            + "," + String(signals[0] + 1) // test thing
+        ;
         Serial.print(s);
-        delay(delay_time);
+        delay(20);
     }
+   delay(10);
 }
